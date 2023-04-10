3e6a0cb3a76218d57cc68cbf1c426964a958163b
Amaresh Barik <96060638+amareshcoding@users.noreply.github.com>
2023-04-10 20:22:39 +0530

    node/express server setup complete

---
+++ index.js
@@ -0,0 +1,38 @@
+import express from 'express';
+import bodyParser from 'body-parser';
+import cors from 'cors';
+import dotenv from 'dotenv';
+import helmet from 'helmet';
+import morgan from 'morgan';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+// Configuration
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+dotenv.config();
+const app = express();
+app.use(express.json());
+app.use(helmet());
+app.use(helmet.crossOriginResourcePolicy({ policy: 'cross-origin' }));
+app.use(morgan('common'));
+app.use(bodyParser.json({ limit: '30mb', extended: true }));
+app.use(bodyParser.urlencoded({ limit: '30mb', extended: true }));
+app.use(cors());
+
+//Home Route
+app.get('/', (req, res) => {
+  res.send('Home Route!');
+});
+
+//Port
+const PORT = process.env.PORT || 8000;
+
+//Server Listening
+app.listen(PORT, async () => {
+  try {
+    console.log(`server listening on port ${PORT}`);
+  } catch (err) {
+    console.log('err: ', err);
+  }
+});
